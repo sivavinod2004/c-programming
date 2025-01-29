5a9fe9b96930efdab31e795550ac448d8c371284
sivavinod2004 <sivapriyaav2028@cs.sjcetpalai.ac.in>
2025-01-29 14:55:06 +0530

    Add files via upload

---
+++ largestofthree.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+  int num1,num2,num3;
+  printf("\n enter three integers");
+  scanf("%d%d%d",&num1,&num2,&num3);
+  if(num1>num2){
+    if(num1>num3){
+      printf("num1 is greater");
+  }}else if(num2>num1 &num2>num3){
+  printf("num2 is greater");
+  }else{
+  printf("num3 is greater");
+  }
+return 0;
+}
