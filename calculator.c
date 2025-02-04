1f47aaee46793a3eed2b6dc7608177992b2aac80
sivavinod2004 <sivapriyaav2028@cs.sjcetpalai.ac.in>
2025-02-04 21:18:55 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main(){
   int num1,num2,calculator;
-  char am;
+  char operator;
   printf("enter two numbers");
   scanf("%d%d",&num1,&num2);
   printf("choose a operator");
   scanf(" %c",&am);
-  switch(am){
+  switch(operator){
   case '+':
   printf("sum :%d",num1+num2);
   break;
